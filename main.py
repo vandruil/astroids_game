f801752ea006082ab71b02fdd96643c04df20866
vandruil <146136334+vandruil@users.noreply.github.com>
2025-07-30 16:50:25 +0200

    changed var window to screen

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import pygame
 
 from constants import *
+from player import Player
 
 
 def main():
     pygame.init()
-    window = pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen = pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
     clock = pygame.time.Clock()
     dt = 0
     while True:
@@ -13,8 +15,8 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-        pygame.Surface.fill(window, (0, 0, 0))
-
+        pygame.Surface.fill(screen, (0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
