20ef9516eeb17c173038c04ee8adbfc16a27a1bf
vandruil <146136334+vandruil@users.noreply.github.com>
2025-07-30 18:36:07 +0200

    changed gameloop to use sprite groups

--- main.py
+++ main.py
@@ -20,9 +20,11 @@ def main():
             if event.type == pygame.QUIT:
                 return
         dt = clock.tick(60) / 1000
+
         pygame.Surface.fill(screen, (0, 0, 0))
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+        for sprite in updatable:
+            sprite.draw(screen)
 
         pygame.display.flip()
 
