b9df51636c3c078e8ea967d0e6f174a1fb449c69
vandruil <146136334+vandruil@users.noreply.github.com>
2025-07-30 19:53:54 +0200

    added asteroid class

---
+++ Asteroid.py
@@ -0,0 +1,14 @@
+import pygame.draw
+
+from circleshape import CircleShape
+
+
+class Asteroid(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+
+    def draw(self, screen: pygame.Surface):
+        pygame.draw.circle(screen, "", self.position, self.radius, 2)
+
+    def update(self, dt):
+        self.velocity += self.velocity * dt
