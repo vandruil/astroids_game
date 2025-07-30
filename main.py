e6f2ac9c7e0cc1b5db92e95a13afbba2e4456f3f
vandruil <146136334+vandruil@users.noreply.github.com>
2025-07-30 15:14:13 +0200

    updated and formated main.py

--- main.py
+++ main.py
@@ -5,17 +5,21 @@ from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 
 def main():
     pygame.init()
-    window = pygame.display.set_mode(size=(SCREEN_WIDTH,SCREEN_HEIGHT))
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH} \nScreen height: {SCREEN_HEIGHT}")
+    window = pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
-         if event.type == pygame.QUIT:
-            return
-        
-        pygame.Surface.fill(window,(0,0,0))
-        
+            if event.type == pygame.QUIT:
+                return
+
+        pygame.Surface.fill(window, (0, 0, 0))
+
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
+        print(clock.get_fps())
+
 
 if __name__ == "__main__":
     main()
