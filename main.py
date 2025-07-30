950d00511187b3c9e1cb5b1170d92cde70abe173
vandruil <146136334+vandruil@users.noreply.github.com>
2025-07-30 20:30:13 +0200

    reordering

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
 import pygame
 
-from Asteroid import Asteroid
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from player import Player
 
 
 def main():
     pygame.init()
-    screen = pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+
+    AsteroidField.containers = updatable  # pyright: ignore[reportAttributeAccessIssue]
     Asteroid.containers = (asteroids, updatable, drawable)  # pyright: ignore[reportAttributeAccessIssue]
+
+    asteroid_field = AsteroidField()
+
     Player.containers = (updatable, drawable)  # pyright: ignore[reportAttributeAccessIssue]
+
     player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)  # noqa: F841
-    clock = pygame.time.Clock()
+
     dt = 0
 
     while True:
@@ -23,9 +32,11 @@ def main():
                 return
         dt = clock.tick(60) / 1000
 
-        pygame.Surface.fill(screen, (0, 0, 0))
+        screen.fill("black")
+
         updatable.update(dt)
-        for sprite in updatable:
+
+        for sprite in drawable:
             sprite.draw(screen)
 
         pygame.display.flip()
