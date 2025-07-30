2bda5124ff3d36553fa074c43baff27cda92adb7
vandruil <146136334+vandruil@users.noreply.github.com>
2025-07-30 18:33:01 +0200

    added updatable and drawable sprite groups

--- main.py
+++ main.py
@@ -7,9 +7,14 @@ from player import Player
 def main():
     pygame.init()
     screen = pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
     clock = pygame.time.Clock()
     dt = 0
+
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
